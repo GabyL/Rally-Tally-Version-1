5f889aa35598e7212a26196de4a1f0ab01be2ba5
3mily <emrcraig@gmail.com>
2015-01-30 01:38:02 +0000

    remote pry, add database

--- config/database.rb
+++ config/database.rb
@@ -4,19 +4,58 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "postgresql",
-    # database: "db/db.sqlite3",
-    database: 'meet_n_eat',#ENV['DATABASE_URL']
-    #  export DATABASE_URL="postgres://USERNAME:PASSWORD@localhost:5432/DATABASENAME"
-    
-    #postgres://user3123:passkja83kd8@ec2-117-21-174-214.compute-1.amazonaws.com:6212/db982398
-    username: 'emily',
-    password: 'emily',
-    host: 'localhost',
-    port: 5432,
-    min_messages: 'error'
+
+
+configure :development do
+ set :database, {
+  adapter: "postgresql",
+  # database: "db/db.sqlite3",
+  database: 'meet_n_eat',#ENV['DATABASE_URL']
+  #  export DATABASE_URL="postgres://USERNAME:PASSWORD@localhost:5432/DATABASENAME"
+  
+  #postgres://user3123:passkja83kd8@ec2-117-21-174-214.compute-1.amazonaws.com:6212/db982398
+  username: 'emily',
+  password: 'emily',
+  host: 'localhost',
+  port: 5432,
+  min_messages: 'error'
   }
+ set :show_exceptions, true
+end
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
+
+
+
+
+
+
+  
+
+  # set :database, {
+  #   adapter: "postgresql",
+  #   # database: "db/db.sqlite3",
+  #   database: 'meet_n_eat',#ENV['DATABASE_URL']
+  #   #  export DATABASE_URL="postgres://USERNAME:PASSWORD@localhost:5432/DATABASENAME"
+    
+  #   #postgres://user3123:passkja83kd8@ec2-117-21-174-214.compute-1.amazonaws.com:6212/db982398
+  #   username: 'emily',
+  #   password: 'emily',
+  #   host: 'localhost',
+  #   port: 5432,
+  #   min_messages: 'error'
+  # }
 
   # ActiveRecord::Base.establish_connection(
   #   adapter: 'postgresql',
