239fb61515447184964ccbc0ceb3400aa30b7051
GabyL <gabriel.leung.607@gmail.com>
2015-02-02 03:38:54 +0000

    fixing time again

--- app/actions.rb
+++ app/actions.rb
@@ -172,7 +172,7 @@ post '/events/confirmation' do # => "send invites" button on pg4.
   if ptminute == 0
     minute = "00"
   else
-    minute == pst_time.min
+    minute = ptminute
   end
 
   true_time = "#{hour}:#{minute} #{am_pm} on #{month} #{pst_time.day}, #{pst_time.year}"
