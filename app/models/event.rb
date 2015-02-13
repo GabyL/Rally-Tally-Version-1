872ca450ababc3809f728c81d994b070b359c581
3mily <emrcraig@gmail.com>
2015-02-13 20:17:35 +0000

    refactor check_vots and check_time

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,26 +5,28 @@ class Event < ActiveRecord::Base
   belongs_to :user
 
   has_many :venues, -> {order("id ASC")}
-  has_many :guests
+  has_many :guests, -> {order("id ASC")}
   has_many :votes
 
   scope :events_without_user, -> {where(user_id: nil)}
 
+  def check_winning_venue(event)
+    event.venues.each do |venue|
+      @winning_venue ||= venue 
+      if venue.votes.count > @winning_venue.votes.count        
+        @winning_venue = venue 
+      end
+    end
+    event.twilio_send_text(event, @winning_venue)
+    event.sent_text = true
+    event.save
+  end
 
   def self.check_votes
-    # if guest.decline == false then send below
     events = Event.where(sent_text: false)
     events.each do |event|
       if event.votes.count == event.guests.count
-        event.venues.each do |venue|
-          @winning_venue ||= venue 
-          if venue.votes.count > @winning_venue.votes.count        
-            @winning_venue = venue 
-          end
-        end
-        event.twilio_send_text(event, @winning_venue)
-        event.sent_text = true
-        event.save
+        check_winning_venue(event)
       end
     end
   end
@@ -33,15 +35,7 @@ class Event < ActiveRecord::Base
     upcoming_events = Event.where(time: 90.minutes.ago..Time.now )
     upcoming_events.each do |event|
       if event.sent_text == false
-        event.venues.each do |venue|
-          @winning_venue ||= venue 
-          if venue.votes.count > @winning_venue.votes.count        
-            @winning_venue = venue 
-          end
-        end
-        event.twilio_send_text(event, @winning_venue)
-        event.sent_text = true
-        event.save
+        check_winning_venue(event)
       end
     end
   end
@@ -55,12 +49,6 @@ class Event < ActiveRecord::Base
     from = "+16043371550"
 
     message_body = ""
-    # location_count = 0
-
-    # venues.reverse.each do |venue|
-    #   location_count += 1
-    #   message_body += "#{location_count}) #{venue.name} "
-    # end
 
     event.guests.each do |guest|
       client.account.messages.create(
