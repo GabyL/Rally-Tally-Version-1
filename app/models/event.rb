8a304b136ae97cb98fd644ab167ae005e51a5581
3mily <emrcraig@gmail.com>
2015-02-19 21:49:00 +0000

    test rake tasks

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,23 +11,21 @@ class Event < ActiveRecord::Base
 
   scope :events_without_user, -> {where(user_id: nil)}
 
-  def check_winning_venue
-    venues.each do |venue|
-      @winning_venue ||= venue 
-      if venue.votes.count > @winning_venue.votes.count        
-        @winning_venue = venue 
-      end
-    end
-    twilio_send_text(self, @winning_venue)
-    sent_text = true
-    save
-  end
 
-  def self.check_votes
+   def self.check_votes
+    # if guest.decline == false then send below
     events = Event.where(sent_text: false)
     events.each do |event|
       if event.votes.count == event.guests.count
-        event.check_winning_venue
+        event.venues.each do |venue|
+          @winning_venue ||= venue 
+          if venue.votes.count > @winning_venue.votes.count        
+            @winning_venue = venue 
+          end
+        end
+        event.twilio_send_text(event, @winning_venue)
+        event.sent_text = true
+        event.save
       end
     end
   end
@@ -36,9 +34,48 @@ class Event < ActiveRecord::Base
     upcoming_events = Event.where(time: 90.minutes.ago..Time.now )
     upcoming_events.each do |event|
       if event.sent_text == false
-        event.check_winning_venue
+        event.venues.each do |venue|
+          @winning_venue ||= venue 
+          if venue.votes.count > @winning_venue.votes.count        
+            @winning_venue = venue 
+          end
+        end
+        event.twilio_send_text(event, @winning_venue)
+        event.sent_text = true
+        event.save
       end
     end
+  end
+
+
+  # def check_winning_venue
+  #   venues.each do |venue|
+  #     @winning_venue ||= venue 
+  #     if venue.votes.count > @winning_venue.votes.count        
+  #       @winning_venue = venue 
+  #     end
+  #   end
+  #   twilio_send_text(self, @winning_venue)
+  #   sent_text = true
+  #   save
+  # end
+
+  # def self.check_votes
+  #   events = Event.where(sent_text: false)
+  #   events.each do |event|
+  #     if event.votes.count == event.guests.count
+  #       event.check_winning_venue
+  #     end
+  #   end
+  # end
+
+  # def self.check_time
+  #   upcoming_events = Event.where(time: 90.minutes.ago..Time.now )
+  #   upcoming_events.each do |event|
+  #     if event.sent_text == false
+  #       event.check_winning_venue
+  #     end
+  #   end
   end
 
   def twilio_send_text(event, winning_venue)
